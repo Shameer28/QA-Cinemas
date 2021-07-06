64884243d39a6a40ebed44acea79addd3f153c97
Andrew-McCall <amccall@qa.com>
2021-07-06 12:14:26 +0100

    AM: Added Mock Payment form

--- qa-cinema/src/frontend/components/props/Payment.jsx
+++ qa-cinema/src/frontend/components/props/Payment.jsx
@@ -2,8 +2,25 @@ const Payment = (props) => {
 
     const {getCart, setCart} = props;
 
-    return ( 
-        <p>Stripe and More Ere Later</p>
+    return (
+        <div>
+            <h3>Payment</h3>
+            <form>
+                <label htmlFor="Name" >Name:</label><br/>
+                <input type="text" id="Name" /><br/>
+
+                <label htmlFor="CardNumber" >Card Number:</label><br/>
+                <input type="number" id="CardNumber" /><br/>
+
+                <label htmlFor="ExpiryData" >Child:</label><br/>
+                <input type="text" id="ExpiryData" /><br/>
+
+                <label htmlFor="CVC" >CVC:</label><br/>
+                <input type="number" id="CVC" /><br/>
+
+                <input type="submit" value="Submit" />
+            </form>
+        </div> 
      );
 }
  
