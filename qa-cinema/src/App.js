e2c0c1ba84971e9e98ced4f0ff9dad0a8526ad77
shameer <SDar@qa.com>
2021-07-06 11:40:38 +0100

    [QAC-59]SD: implemented routes for all pages in navbar

--- qa-cinema/src/App.js
+++ qa-cinema/src/App.js
@@ -2,15 +2,65 @@ import './App.css';
 import HomePage from './components/HomePage';
 import NavBar from './components/props/navbar';
 import Footer from './components/props/Footer';
+import { BrowserRouter, Route } from "react-router-dom";
+import AllGalleryPage from './components/AllGalleryPage';
+import OpeningTimesPage from './components/OpeningTimesPage';
+import Classification from './components/Classification';
+import PlacesToGo from './components/PlacesToGo';
+import DiscussionBoard from './components/DiscussionBoard';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <HomePage />
-      <Footer />
-    </div>
+      <BrowserRouter>
+        <NavBar />
+
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="/nowshowing">
+          <AllGalleryPage />
+        </Route>
+
+        {/* <Route path="/newreleases">//OLIVER
+
+        </Route> */}
+
+        <Route path="/openingtimes">
+          <OpeningTimesPage />
+        </Route>
+
+        <Route path="/classification">
+          <Classification />
+        </Route>
+
+        <Route path="/placestogo">
+          <PlacesToGo />
+        </Route>
+        {/* 
+        <Route path="/gettingthere"> //HAARIS
+
+        </Route> */}
+
+        <Route path="/discussionboard">
+          <DiscussionBoard />
+        </Route>
+
+        <Route path="/aboutpage">
+          <AboutPage />
+        </Route>
+
+        <Route path="/contactpage">
+          <ContactPage />
+        </Route>
+
+        <Footer />
+      </BrowserRouter>
+    </div >
   );
 }
 
---
+++ qa-cinema/src/components/PlacesToGo.jsx
@@ -0,0 +1,9 @@
+const PlacesToGo = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default PlacesToGo;
