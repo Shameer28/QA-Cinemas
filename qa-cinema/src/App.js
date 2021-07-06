cc0367e1f7c8e2283d2cea850d4b1a884f80b6b3
shameer <SDar@qa.com>
2021-07-06 10:47:46 +0100

    SD: sorting out folders and fixing imports

--- qa-cinema/src/App.js
+++ qa-cinema/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import HomePage from './frontend/components/props/HomePage';
-import NavBar from './frontend/components/props/navbar';
-import Footer from './frontend/components/props/Footer';
+import HomePage from './components/HomePage';
+import NavBar from './components/props/navbar';
+import Footer from './components/props/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
--- qa-cinema/src/components/HomePage.jsx
+++ qa-cinema/src/components/HomePage.jsx
@@ -1,8 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Carousels from './Carousel'
-
+import Carousels from './props/Carousel';
 import image1 from './img/BlackWidow.jpg'
 import image2 from './img/Fast9.jpg'
 import image3 from './img/JamesBond.jpg'
@@ -11,18 +10,18 @@ const HomePage = () => {
     return (
         <div>
             <h3>Homepage</h3>
-            <Carousels/>
+            <Carousels />
             <h3>Top films</h3>
             <Container>
                 <Row md={4}>
                     <Col xs={3}>
-                        <img src={image1} alt="BlackWidow" width="200"/>
+                        <img src={image1} alt="BlackWidow" width="200" />
                     </Col>
-                    <Col xs={6}><img src={image2} alt="Fast9" width="200"/></Col>
-                    <Col><img src={image3} alt="JamesBond" width="200"/></Col>
+                    <Col xs={6}><img src={image2} alt="Fast9" width="200" /></Col>
+                    <Col><img src={image3} alt="JamesBond" width="200" /></Col>
                 </Row>
             </Container>
-            
+
         </div>
     );
 }
