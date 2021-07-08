88f543b0b0afbd561f61528c3beeb7aaedba58e3
shameer <SDar@qa.com>
2021-07-08 16:44:50 +0100

    SD: changed styling on navbar

--- qa-cinema/src/components/OpeningTimesPage/OpeningTimesPage.jsx
+++ qa-cinema/src/components/OpeningTimesPage/OpeningTimesPage.jsx
@@ -1,11 +1,13 @@
 import Tables from "./Tables";
 import { Container, Image } from 'react-bootstrap'
 import image1 from './../img/openingtimes.png'
+import './OpeningTimesPage.css'
 
 const OpeningTimesPage = () => {
     return (
         <div>
             < Image src={image1} alt="About us photo" width="100%" /> <br />
+            <br />
 
             <Container>
                 <h1>QA Cinemas' Opening Hours :</h1><br />
