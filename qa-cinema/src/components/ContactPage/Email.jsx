51d09a27502c217745aebe5f20ea4c452f514149
Haaris Moghal <haaris9876@hotmail.co.uk>
2021-07-08 13:08:33 +0100

    [QAC-75] HM: Fixed the styling on Contact Us page

--- qa-cinema/src/components/ContactPage/Email.jsx
+++ qa-cinema/src/components/ContactPage/Email.jsx
@@ -1,14 +1,15 @@
-import Form from 'react-bootstrap/Form'
-//import axios from 'axios';
-import { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"></link>
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from 'react-bootstrap/Card'
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 const Email = () => {
     const [Email, setEmail] = useState('');
     const [FirstName, setFirstName] = useState('');
     const [LastName, setLastName] = useState('');
     const [TextArea, setTextArea] = useState('');
+
 
 
 
@@ -19,6 +20,7 @@ const Email = () => {
         const dataToSend = JSON.stringify({ Email, FirstName, LastName, TextArea });
         console.log("data has been sent");
         console.log(dataToSend);
+
 
 
         //Setting the states back to ''
@@ -46,31 +48,40 @@ const Email = () => {
 
     }
     return (
-        <Container>
-            
-                <h3> Please fill out the form to submit a request</h3>
-                <br></br>
-                <Form onSubmit={handleSubmit} >
-                    <Form.Group>
-                        <Form.Label><u>Email address</u></Form.Label>
-                        <Form.Control id="Email" type="email" className="form-control" placeholder="name@example.com" value={Email} pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
-                            required onChange={(e) => { setEmail(e.target.value) }} />
-                    </Form.Group>
-                    <Form.Group >
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control id="FirstName" type="text" className="form-control" required minLength={3} placeholder="First Name" value={FirstName} onChange={(e) => { setFirstName(e.target.value) }} />
+        <div class="background">
+            <Container>
+                <Card className="text-center">
+                    <Card.Header>Contact US</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Please fill out the form to submit a request</Card.Title>
+                        <Card.Text>
+                            <Form onSubmit={handleSubmit} >
+                                <Form.Group>
+                                    <Form.Label><u>Email address</u></Form.Label>
+                                    <Form.Control id="Email" type="email" className="form-control" placeholder="name@example.com" value={Email} pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+                                        required onChange={(e) => { setEmail(e.target.value) }} />
+                                </Form.Group>
+                                <Form.Group >
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control id="FirstName" type="text" className="form-control" required minLength={3} placeholder="First Name" value={FirstName} onChange={(e) => { setFirstName(e.target.value) }} />
 
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control id="LastName" type="text" className="form-control" required minLength={3} placeholder="Last Name" value={LastName} onChange={(e) => { setLastName(e.target.value) }} />
-                    </Form.Group>
-                    <Form.Group >
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control id="TextArea" as="textarea" className="form-control" required minLength={3} rows={3} value={TextArea} onChange={(e) => { setTextArea(e.target.value) }} />
-                    </Form.Group>
-                    <button type="submit"> submit</button>
-                </Form>
-            
-        </Container>
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control id="LastName" type="text" className="form-control" required minLength={3} placeholder="Last Name" value={LastName} onChange={(e) => { setLastName(e.target.value) }} />
+                                </Form.Group>
+                                <Form.Group >
+                                    <Form.Label>Message</Form.Label>
+                                    <Form.Control id="TextArea" as="textarea" className="form-control" required minLength={3} rows={3} value={TextArea} onChange={(e) => { setTextArea(e.target.value) }} />
+                                </Form.Group>
+                                <br />
+                                <button type="submit"> submit</button>
+                            </Form>
+                        </Card.Text>
+
+                    </Card.Body>
+                    <Card.Footer className="text-muted"></Card.Footer>
+                </Card>
+            </Container>
+        </div>
     );
 }
 export default Email;
